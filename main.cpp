37a4284922121935151cfcfbc6561644d007b0d3
NguyenLeNgocHa11221919 <128773310+NguyenLeNgocHa11221919@users.noreply.github.com>
2023-06-22 23:04:20 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,46 +3,60 @@ Copy đoạn code vào file này
 **Lưu ý không đổi tên file hoặc định dạng file
 **Nộp thành công sẽ có tích xanh ở ngoài repository
 *******************************************************************************/
-#include<stdio.h>
-#include<string.h>
-struct student
-{
-    char name[50];
+#include <stdio.h>
+
+struct student {
+    char name[20];
     int eng;
     int math;
     int phys;
-};  
-void get_information(int n,student stulist[])
-{
-    for (int i =0; i<4;i++)
-    {    
-        // fgets(stulist[i].name,50,stdin);
-        gets(stulist[i].name);
-        //getcharP();
-        // stulist[i].name[strlen(stulist[i].name)-1] = 0;
-        scanf("%d%d%d",&stulist[i].eng,&stulist[i].math,&stulist[i].phys);
-        //fgetc(stdin);
-        getchar();
-    }  
-}
-void print_information(int n, student stulist[])
-{
-    for ( int i =0; i<4 ; i++)
-    {
-        printf("\nThong tin cua sinh vien: %d ", i+1);
-        printf("\nName:%s",stulist[i].name);
-        printf("\nenglish score:%d",stulist[i].eng);
-        printf("\nmath score:%d",stulist[i].math);
-        printf("\nphysic score:%d",stulist[i].phys);
+    
+};
+int main() {
+    struct student students[2];
+    int i;
+
+    // Accept information for each student
+    for (i = 0; i < 2; i++) {
+        printf("Enter name of student %d: ", i+1);
+        scanf("%s", students[i].name);
+        printf("Enter marks in English for student %d: ", i+1);
+        scanf("%d", &students[i].eng);
+        printf("Enter marks in Math for student %d: ", i+1);
+        scanf("%d", &students[i].math);
+        printf("Enter marks in Physics for student %d: ", i+1);
+        scanf("%d", &students[i].phys);
     }
-}
-int main()
-{   int n;
-    printf("So sinh vien la");
-    scanf("%d",&n);
-    getchar();
-    student stulist[50];
-    get_information(n,stulist);
-    print_information(n,stulist);
-    return 0;
-}
+
+    // Display information of all students
+    printf("\nInformation of all students:\n");
+    for (i = 0; i < 2; i++) {
+        printf (" {\"%s\", %d, %d, %d}\n", students[i].name, students[i].eng, students[i].math, students[i].phys);
+    }
+        float  x;
+        char grade;
+    for (int i = 0; i<2; i++) {
+             if (strcmp( ten_nhap_vao, students[i].name) == 0) {
+                     //Tính điểm và in grade
+              }
+    
+           x = (students[i].eng + students[i].math + students[i].phys) / 3;
+    
+        if (x >= 90 && x <= 100) {
+            grade = 'S';
+        } else if (x >= 80 && x < 90) {
+            grade = 'A';
+        } else if (x >= 70 && x < 80) {
+            grade = 'B';
+        } else if (x >= 60 && x < 70) {
+            grade = 'C';
+        } else {
+            grade = 'D';
+        }
+
+        printf("Grade: %c\n", grade);
+
+        return 0;
+    }
+             
+             }
